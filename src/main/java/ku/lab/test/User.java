f2e3dc7f013e71b2ca347db1e6fb1204c982912c
aphisit prasertvesyakorn <aphisit.pra@ku.th>
2023-07-27 10:23:22 +0700

    User Add AccountName

--- src/main/java/ku/lab/test/User.java
+++ src/main/java/ku/lab/test/User.java
@@ -3,9 +3,11 @@ package ku.lab.test;
 public class User {
     private String userName;
     private String iD;
+    private String accountName;
 
     public User() {
         userName = "Alex";
+        accountName = "JJ";
         iD = "00125";
     }
 }
