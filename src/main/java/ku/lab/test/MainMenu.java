242f08d543af71c3d77507d3ae8af1233885319e
nammeyyy <nutpawee.k@ku.th>
2023-07-27 11:14:26 +0700

    Add getter and Setter

--- src/main/java/ku/lab/test/MainMenu.java
+++ src/main/java/ku/lab/test/MainMenu.java
@@ -3,4 +3,20 @@ package ku.lab.test;
 public class MainMenu {
     int x =0;
     int y = 1;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
