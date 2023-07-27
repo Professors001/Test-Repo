ad674ed5a1b07d3de855b26f87e2ade0805b7ffa
aphisit prasertvesyakorn <aphisit.pra@ku.th>
2023-07-27 10:19:56 +0700

    UserClass

---
+++ src/main/java/ku/lab/test/User.java
@@ -0,0 +1,11 @@
+package ku.lab.test;
+
+public class User {
+    private String userName;
+    private String iD;
+
+    public User() {
+        userName = "Alex";
+        iD = "00125";
+    }
+}
